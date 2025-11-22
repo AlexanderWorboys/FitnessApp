28c7bc8927798fa4a3a23dcc75b3f56321e439a7
Alex <aworboys2000@gmail.com>
2025-11-22 16:23:17 +0000

    Comment out old workout store

--- src/store/WorkoutStore.ts
+++ src/store/WorkoutStore.ts
@@ -1,189 +1,189 @@
-import { create } from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
-import { Workout, WorkoutExercise, WorkoutTemplate } from "../types/workout";
-import * as SQLite from "expo-sqlite"
-import AsyncStorage from "@react-native-async-storage/async-storage"
-import { getAllWorkouts, insertWorkout } from "../database/workoutDb";
-import { createEmptySet, getColumnsForType } from "../data/tableColumns";
-import { findPreviousExerciseSets } from "../utils/findPreviousExerciseSets";
+// import { create } from "zustand";
+// import { createJSONStorage, persist } from "zustand/middleware";
+// import { Workout, WorkoutExercise, WorkoutTemplate } from "../types/workout";
+// import * as SQLite from "expo-sqlite"
+// import AsyncStorage from "@react-native-async-storage/async-storage"
+// import { getAllWorkouts, insertWorkout } from "../database/workoutDb";
+// import { createEmptySet, getColumnsForType } from "../data/tableColumns";
+// import { findPreviousExerciseSets } from "../utils/findPreviousExerciseSets";
 
 
 
-interface WorkoutStore {
-  activeWorkout: Workout | null
-  workoutHistory: Workout[]
-  templates: WorkoutTemplate[]
+// interface WorkoutStore {
+//   activeWorkout: Workout | null
+//   workoutHistory: Workout[]
+//   templates: WorkoutTemplate[]
 
-  // Create
-  setTemplates: (templates: WorkoutTemplate[]) => void
-  startWorkout: (name: string, templateId?: string) => void
-  addExercise: (exercise: WorkoutExercise) => void
-  addTemplate: (template: WorkoutTemplate) => void
+//   // Create
+//   setTemplates: (templates: WorkoutTemplate[]) => void
+//   startWorkout: (name: string, templateId?: string) => void
+//   addExercise: (exercise: WorkoutExercise) => void
+//   addTemplate: (template: WorkoutTemplate) => void
 
-  // Reads
-  loadWorkoutHistory: () => void;
-  loadTemplate: (tempalteId: string) => void
+//   // Reads
+//   loadWorkoutHistory: () => void;
+//   loadTemplate: (tempalteId: string) => void
 
-  // Updates
-  updateWorkoutName: (id: string, name: string) => void
-  updateExercise: (id: string, updatedExercise: WorkoutExercise) => void
-  completeWorkout: () => void
+//   // Updates
+//   updateWorkoutName: (id: string, name: string) => void
+//   updateExercise: (id: string, updatedExercise: WorkoutExercise) => void
+//   completeWorkout: () => void
 
-  // Delete (cancel)
-  cancelWorkout: () => void
-  deleteExercise: (id: string) => void
-}
+//   // Delete (cancel)
+//   cancelWorkout: () => void
+//   deleteExercise: (id: string) => void
+// }
 
 
-export const useOldWorkoutStore = create<WorkoutStore>()(
-  persist(
-    (set, get) => ({
-      activeWorkout: null,
-      workoutHistory: [],
-      templates: [],
+// export const useOldWorkoutStore = create<WorkoutStore>()(
+//   persist(
+//     (set, get) => ({
+//       activeWorkout: null,
+//       workoutHistory: [],
+//       templates: [],
 
-      setTemplates: (templates) => set({ templates }),
+//       setTemplates: (templates) => set({ templates }),
 
-      startWorkout: (name, templateId) => {
-        const template = templateId
-          ? get().templates.find((t) => t.id === templateId)
-          : null
+//       startWorkout: (name, templateId) => {
+//         const template = templateId
+//           ? get().templates.find((t) => t.id === templateId)
+//           : null
 
-        const newWorkout: Workout = {
-          id: Date.now().toString(),
-          name,
-          startTime: Date.now(),
-          exercises: template ? [...template.exercises] : [],
-          completed: false,
-          fromTemplate: templateId,
-        }
+//         const newWorkout: Workout = {
+//           id: Date.now().toString(),
+//           name,
+//           startTime: Date.now(),
+//           exercises: template ? [...template.exercises] : [],
+//           completed: false,
+//           fromTemplate: templateId,
+//         }
 
-        set({ activeWorkout: newWorkout })
-      },
+//         set({ activeWorkout: newWorkout })
+//       },
 
-      loadWorkoutHistory: async () => {
-        const workouts = await getAllWorkouts();
-        set({ workoutHistory: workouts });
-      },
+//       loadWorkoutHistory: async () => {
+//         const workouts = await getAllWorkouts();
+//         set({ workoutHistory: workouts });
+//       },
 
-      completeWorkout: async () => {
-        const { activeWorkout } = get()
-        if (!activeWorkout) return
+//       completeWorkout: async () => {
+//         const { activeWorkout } = get()
+//         if (!activeWorkout) return
 
-        const completeWorkout = {
-          ...activeWorkout,
-          completed: true,
-          endTime: Date.now(),
-        }
+//         const completeWorkout = {
+//           ...activeWorkout,
+//           completed: true,
+//           endTime: Date.now(),
+//         }
 
-        insertWorkout(completeWorkout);
+//         insertWorkout(completeWorkout);
 
-        const workouts = await getAllWorkouts();
-        set({ activeWorkout: null, workoutHistory: workouts })
-      },
+//         const workouts = await getAllWorkouts();
+//         set({ activeWorkout: null, workoutHistory: workouts })
+//       },
 
-      // addExercise: (exercise) => {
-      //   const { activeWorkout } = get()
-      //   if (!activeWorkout) return
-      //   set({
-      //     activeWorkout: {
-      //       ...activeWorkout,
-      //       exercises: [...activeWorkout.exercises, exercise],
-      //     },
-      //   })
-      // },
-      addExercise: (exercise) =>
-        set((state) => {
-          if (!state.activeWorkout) return state;
+//       // addExercise: (exercise) => {
+//       //   const { activeWorkout } = get()
+//       //   if (!activeWorkout) return
+//       //   set({
+//       //     activeWorkout: {
+//       //       ...activeWorkout,
+//       //       exercises: [...activeWorkout.exercises, exercise],
+//       //     },
+//       //   })
+//       // },
+//       addExercise: (exercise) =>
+//         set((state) => {
+//           if (!state.activeWorkout) return state;
 
-          // This will need updating to workout id in the future
-          const previous = findPreviousExerciseSets(exercise.name, state.workoutHistory);
+//           // This will need updating to workout id in the future
+//           const previous = findPreviousExerciseSets(exercise.name, state.workoutHistory);
 
-          const newExercise: WorkoutExercise = {
-            id: Date.now().toString(),
-            name: exercise.name,
-            type: exercise.type,
-            columns: getColumnsForType(exercise.type),
-            sets: [createEmptySet(exercise.type, 1)],
-            previous: previous ?? [],
-          };
+//           const newExercise: WorkoutExercise = {
+//             id: Date.now().toString(),
+//             name: exercise.name,
+//             type: exercise.type,
+//             columns: getColumnsForType(exercise.type),
+//             sets: [createEmptySet(exercise.type, 1)],
+//             previous: previous ?? [],
+//           };
 
-          return {
-            activeWorkout: {
-              ...state.activeWorkout,
-              exercises: [...state.activeWorkout.exercises, newExercise],
-            },
-          };
-        }),
+//           return {
+//             activeWorkout: {
+//               ...state.activeWorkout,
+//               exercises: [...state.activeWorkout.exercises, newExercise],
+//             },
+//           };
+//         }),
 
-      updateExercise: (id, updatedExercise) => {
-        const { activeWorkout } = get()
-        if (!activeWorkout) return
-        set({
-          activeWorkout: {
-            ...activeWorkout,
-            exercises: activeWorkout.exercises.map((ex) =>
-              ex.id === id ? updatedExercise : ex
-            ),
-          },
-        })
-      },
+//       updateExercise: (id, updatedExercise) => {
+//         const { activeWorkout } = get()
+//         if (!activeWorkout) return
+//         set({
+//           activeWorkout: {
+//             ...activeWorkout,
+//             exercises: activeWorkout.exercises.map((ex) =>
+//               ex.id === id ? updatedExercise : ex
+//             ),
+//           },
+//         })
+//       },
 
-      updateWorkoutName: (id, name) => {
-        const { activeWorkout } = get()
-        if (!activeWorkout || activeWorkout.id !== id) return
+//       updateWorkoutName: (id, name) => {
+//         const { activeWorkout } = get()
+//         if (!activeWorkout || activeWorkout.id !== id) return
 
-        set({
-          activeWorkout: {
-            ...activeWorkout,
-            name, // overwrite just the name
-          },
-        })
-      },
+//         set({
+//           activeWorkout: {
+//             ...activeWorkout,
+//             name, // overwrite just the name
+//           },
+//         })
+//       },
 
-      loadTemplate: (templateId) => {
-        const template = get().templates.find((t) => t.id === templateId)
-        if (!template) return
-        set({
-          activeWorkout: {
-            id: Date.now().toString(),
-            name: template.name,
-            startTime: Date.now(),
-            exercises: template.exercises.map((ex) => ({
-              ...ex,
-              sets: ex.sets.map((s) => ({ ...s, complete: false })),
-            })),
-            completed: false,
-            fromTemplate: templateId,
-          },
-        })
-      },
+//       loadTemplate: (templateId) => {
+//         const template = get().templates.find((t) => t.id === templateId)
+//         if (!template) return
+//         set({
+//           activeWorkout: {
+//             id: Date.now().toString(),
+//             name: template.name,
+//             startTime: Date.now(),
+//             exercises: template.exercises.map((ex) => ({
+//               ...ex,
+//               sets: ex.sets.map((s) => ({ ...s, complete: false })),
+//             })),
+//             completed: false,
+//             fromTemplate: templateId,
+//           },
+//         })
+//       },
 
-      addTemplate: (template) => {
-        set((state) => ({
-          templates: [...state.templates, { ...template, createdAt: Date.now() }],
-        }))
-      },
+//       addTemplate: (template) => {
+//         set((state) => ({
+//           templates: [...state.templates, { ...template, createdAt: Date.now() }],
+//         }))
+//       },
 
-      cancelWorkout: () => {
-        set({ activeWorkout: null });
-      },
+//       cancelWorkout: () => {
+//         set({ activeWorkout: null });
+//       },
 
-      deleteExercise: (id) => {
-        const { activeWorkout } = get()
-        if(!activeWorkout) return
-        set({
-          activeWorkout: {
-            ...activeWorkout,
-            exercises: activeWorkout.exercises.filter((ex) => ex.id !== id),
-          },
-        })
-      }
+//       deleteExercise: (id) => {
+//         const { activeWorkout } = get()
+//         if(!activeWorkout) return
+//         set({
+//           activeWorkout: {
+//             ...activeWorkout,
+//             exercises: activeWorkout.exercises.filter((ex) => ex.id !== id),
+//           },
+//         })
+//       }
 
-    }),
-    {
-      name: "workout-storage",
-      storage: createJSONStorage(() => AsyncStorage),
-    }
-  )
-)
+//     }),
+//     {
+//       name: "workout-storage",
+//       storage: createJSONStorage(() => AsyncStorage),
+//     }
+//   )
+// )
